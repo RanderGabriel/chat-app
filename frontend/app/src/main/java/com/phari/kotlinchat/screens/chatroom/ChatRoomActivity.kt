5753c3deab48f1c667ffcbb161a77de410a47de6
RanderGabriel <randergabriel98@gmail.com>
2020-05-16 22:19:24 -0300

    Created ChatRoomActivity

--- frontend/app/src/main/java/com/phari/kotlinchat/screens/chatroom/ChatRoomActivity.kt
+++ frontend/app/src/main/java/com/phari/kotlinchat/screens/chatroom/ChatRoomActivity.kt
@@ -3,10 +3,22 @@ package com.phari.kotlinchat.screens.chatroom
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.phari.kotlinchat.R
+import com.phari.kotlinchat.components.chatmessage.ChatMessage
+import kotlinx.android.synthetic.main.activity_chat_room.*
 
 class ChatRoomActivity: AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat_room)
+        sendButton.setOnClickListener {
+            onSendMessagePressed()
+        }
+    }
+
+    fun onSendMessagePressed() {
+        val text = chatMessageField.text.toString()
+        messagesContainer.addView(ChatMessage(this, text))
+        chatMessageField.text.clear()
     }
 }
